eb9a4b73d8fc0548f37307869d3fa10190f37be2
Chris Wu <cwu0569@gmail.com>
2020-08-18 00:20:15 -0700

    added some styling

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ const libraryContainer = document.querySelector('.grid');
 Book.prototype.initialReadToggle = function() {
     let string;
     if (this.read) {
-        string = 'checked';
+        string = "checked";
     } else {
-        string = '';
+        string = "";
     }
     return string;
 };
@@ -79,32 +79,30 @@ function render() {
 
         libraryContainer.appendChild(card);   
     })
-}
-
-// render();
+};
 
 const addBtn = document.querySelector('#add-btn');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('#close-btn');
 const newBookForm = document.querySelector('#new-book-form');
+const submitBtn = document.getElementById('submit-btn');
 
 addBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
-})
+});
 
 closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
-})
+});
 
 newBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     // read in title, author, pages, read status form values
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let readStatus = document.getElementById('read').checked;
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const readStatus = document.getElementById('read').checked;
     const newBook = new Book(title, author, pages, readStatus);
-    console.log(newBook);
     addBookToLibrary(newBook);
     saveToStorage(myLibrary);
     render();
@@ -112,7 +110,27 @@ newBookForm.addEventListener('submit', (e) => {
     modal.style.display = 'none';
     //reset form values
     newBookForm.reset();
-})
+    submitBtn.setAttribute('style', 'opacity: 0.25');
+});
+
+function formValidation() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    console.log(title,author,pages);
+    if (title !== '' && author !== '' && pages !== '') {
+        //apply active state css
+        submitBtn.setAttribute('style', 'opacity: 1; border: 1.75px solid #5898DD;');
+    } else {
+        // grey it out again
+        submitBtn.setAttribute('style', 'opacity: 0.25');
+    }
+};
+
+document.addEventListener('keyup', e => {
+    formValidation();
+});
+
 
 Book.prototype.toggleRead = function() {
     this.read ? this.read = false: this.read = true;
@@ -126,6 +144,7 @@ libraryContainer.addEventListener('click', e => {
         const index = bookDOM.dataset.index;
         //remove book from array
         myLibrary.splice(index, 1);
+        saveToStorage(myLibrary);
         // update UI
         render();
     } else if (readBtn) {
@@ -149,25 +168,15 @@ function saveToStorage(libraryArray) {
 };
 
 window.addEventListener('load', () => {
-    // check if theres a library in local storage already
-    // const library = JSON.parse(localStorage.getItem('library'));
-    // if (!library) {
-    //     myLibrary = [];
-    // } else {
-    //     myLibrary = library;
-    //     render();
-    // }
-
     if (!localStorage.getItem('library')) {
         myLibrary = [];
     } else {
         myLibrary = JSON.parse(localStorage.getItem('library'));
-        // const newBook = new Book(title, author, pages, readStatus);
-        myLibrary = myLibrary.map(book => {
+
         // re-create the books with the Book constructor
+        myLibrary = myLibrary.map(book => {
         return new Book(book.title, book.author, book.pages, book.read)
         });
         render();
     }
-    // console.log(myLibrary);
 });
